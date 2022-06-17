3e975237d8f7f890618ecab06168e38d55f1bb49
Maximilien DI DIO <74690920+prx0@users.noreply.github.com>
2022-06-17 19:17:18 +0200

    wip: Add more operations to the CPU

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ struct CPU {
     registers: [u8; 16],
     position_in_memory: usize, // Rust allows usize for indexing, so we'll use it over u16
     memory: [u8; 0x1000],
+    stack: [u16; 16], // Maximum heigtht is 16, raise a stack overflow after 16 nested function calls
+    stack_pointer: usize, // Index values within the stack
 }
 
 impl Default for CPU {
@@ -10,6 +12,8 @@ impl Default for CPU {
             registers: [0; 16],
             position_in_memory: 0,
             memory: [0; 0x1000],
+            stack: [0; 16],
+            stack_pointer: 0,
         }
     }
 }
@@ -35,8 +39,12 @@ impl CPU {
             let opcode = self.read_opcode();
             self.position_in_memory += 2;
             let (c, x, y, d) = CPU::decoding_opcode(opcode);
+            let nnn = opcode & 0x0FFF;
+            // let kk = (opcode & 0x00FF) as u8;
             match (c, x, y, d) {
                 (0, 0, 0, 0) => { return; }
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 // CPU RIA/1 match 0x8__4 to perform an addition
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
@@ -59,15 +67,42 @@ impl CPU {
             self.registers[0xF] = 0;
         }
     }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow!");
+        }
+
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow");
+        }
+
+        self.stack_pointer -= 1;
+        let call_addr = self.stack[self.stack_pointer];
+        self.position_in_memory = call_addr as usize;
+    }
 }
 
 fn main() {
+    let add_twice: [u8; 6] = [
+        0x80, 0x14,
+        0x80, 0x14,
+        0x00, 0xEE,
+    ];
+
     let mut cpu = CPU::default();
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     // 8 signifies that the operation involves two registers
     // x maps to cpu.registers[x]
@@ -75,13 +110,14 @@ fn main() {
     // 4 indicates the addition
 
     let mem = &mut cpu.memory;
-    mem[0] = 0x80; mem[1] = 0x14; // Load opcode 0x8014, ask to adds register 1 to register 0
-    mem[2] = 0x80; mem[3] = 0x24; // Load opcode 0x8024, ask to adds register 2 to register 0
-    mem[4] = 0x80; mem[5] = 0x34; // Load opcode 0x8034, ask to adds register 3 to register 0
+    mem[0x000] = 0x21; mem[0x001] = 0x00;
+    mem[0x002] = 0x21; mem[0x003] = 0x00;
+    mem[0x004] = 0x00; mem[0x005] = 0x00;
 
+    mem[0x100..0x106].copy_from_slice(&add_twice);
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
 
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    println!("5 + (2 * 10) + (2 * 10) = {}", cpu.registers[0]);
 }
